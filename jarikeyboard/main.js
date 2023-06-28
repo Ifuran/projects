8e47362a92a32834d8df06f88a29b88f05988759
unknown <silalahifran@gmail.com>
2023-06-28 12:57:56 +0700

    tambah animasi

--- jarikeyboard/main.js
+++ jarikeyboard/main.js
@@ -4,4 +4,15 @@ let navlist = document.querySelector('.navlist');
 menu.onclick = () => {
 	navlist.classList.toggle('open');
 	menu.classList.toggle('bx-x');
-}
+};
+
+const sr = ScrollReveal({
+	distance: '65px',
+	duration: 2600,			
+	reset: true
+});
+
+sr.reveal('.hero-text', {delay: 100, origin: 'top'});
+sr.reveal('.hero-img', {delay: 200, origin: 'top'});
+sr.reveal('.icons', {delay: 300, origin: 'left'});
+sr.reveal('.chat-us', {delay: 350, origin: 'right'});
